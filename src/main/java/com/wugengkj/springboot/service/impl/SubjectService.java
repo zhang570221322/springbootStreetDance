60eef89eb8ae72c0561a66520cf1671fb06d4206
zhang570221322 <570221322@qq.com>
2018-05-10 15:52:20 +0800

    题库的算法方法

--- src/main/java/com/wugengkj/springboot/service/impl/SubjectService.java
+++ src/main/java/com/wugengkj/springboot/service/impl/SubjectService.java
@@ -1,12 +1,16 @@
 package com.wugengkj.springboot.service.impl;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.baomidou.mybatisplus.mapper.Wrapper;
+
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.wugengkj.springboot.common.enums.UserStatus;
 import com.wugengkj.springboot.entity.Subject;
 import com.wugengkj.springboot.mapper.SubjectMapper;
 import com.wugengkj.springboot.service.ISubjectService;
+
+import com.wugengkj.springboot.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +25,8 @@ import java.util.List;
 @Service
 @Slf4j
 public class SubjectService extends ServiceImpl<SubjectMapper, Subject> implements ISubjectService {
-
+    @Autowired
+    private IUserService userService;
     @Override
     @Cacheable(value = "queryList", key = "#p0")
     public List<Subject> queryList(int key) {
@@ -38,6 +43,14 @@ public class SubjectService extends ServiceImpl<SubjectMapper, Subject> implemen
     public List<Subject> getRandomList(String openid) {
         List<Subject> subjectList = this.queryList(-1);
         List<Subject> list = new ArrayList<>();
-        return list;
+        int i = userService.queryUserStatus(openid);
+        if(i== UserStatus.USER_ANSWERING.getCode()){
+            //从数据中得到已经分配的
+            return list;
+        }else {
+            //随机生成题目
+            return list;
+        }
+
     }
 }
