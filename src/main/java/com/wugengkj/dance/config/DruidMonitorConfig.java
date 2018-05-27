8ac6dc9c2cb2e179b95504c6d70c1f37582bc956
leaf <806569552@qq.com>
2018-05-27 15:21:38 +0800

    提交

--- src/main/java/com/wugengkj/dance/config/DruidMonitorConfig.java
+++ src/main/java/com/wugengkj/dance/config/DruidMonitorConfig.java
@@ -2,17 +2,12 @@ package com.wugengkj.dance.config;
 
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
-import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
 import lombok.Data;
-import org.springframework.aop.Advisor;
-import org.springframework.aop.support.DefaultPointcutAdvisor;
-import org.springframework.aop.support.JdkRegexpMethodPointcut;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 /**
--- src/main/java/com/wugengkj/dance/config/WebMvcConfig.java
+++ src/main/java/com/wugengkj/dance/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package com.wugengkj.dance.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -12,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  * <p>date: 2018-05-07 19:55</p>
  * <p>version: 1.0</p>
  */
-@Profile({"dev"})
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
